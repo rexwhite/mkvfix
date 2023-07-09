ae3d7573f5f70b9650234f71c991d706a36d45e2
Rex White <rexwhite@gmail.com>
2023-07-08 21:33:34 -0400

    Added panedwindow for treeview and ConsoleText

--- ui.py
+++ ui.py
@@ -8,15 +8,19 @@ filename = ''
 
 
 def choose_file():
-    global filename, file_name
+    global filename, entry_filename
     filename = filedialog.askopenfilename()
-    file_name.configure(state=ACTIVE)
-    file_name.delete(0, END)
-    file_name.insert(0, filename)
-    file_name.configure(state=DISABLED)
+    if filename:
+        entry_filename.configure(state=ACTIVE)
+        entry_filename.delete(0, END)
+        entry_filename.insert(0, filename)
+        entry_filename.configure(state=DISABLED)
 
 
 class ConsoleText(Text):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
     def write(self, message):
         self.configure(state=NORMAL)
         self.insert(END, message)
@@ -28,38 +32,53 @@ def test():
     txt_output.write("Some line of output...\n")
 
 
+# root window
 root = Tk()
 root.title('MKV Tool')
 root.minsize(800, 450)
 root.geometry('1000x600+350+150')
 root.config(bg='lightgrey')
 root.resizable(True, True)
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
 
+# frames
 frame_top = Frame(root)
-frame_top.pack(expand=False, anchor=W, fill=BOTH, padx=3, pady=3)
+frame_top.pack(expand=False, anchor=N, fill=BOTH, padx=3, pady=3)
 
-frame_mid = Frame(root, borderwidth=3, relief=RIDGE)
-frame_mid.pack(expand=True, anchor=S, fill=BOTH, padx=4, pady=1)
+frame_center = Panedwindow(root, orient=VERTICAL)
+frame_center.pack(expand=True, fill=BOTH)
+
+frame_log = Frame(frame_center, borderwidth=5, relief=RIDGE)
+frame_center.add(frame_log)
+# frame_log.pack(expand=True, anchor=N, fill=BOTH)
+
+frame_tracks = Frame(frame_center, borderwidth=2, relief=RIDGE)
+frame_center.add(frame_tracks)
+# frame_tracks.pack(expand=True, anchor=N, fill=BOTH, side=TOP, padx=4, pady=1)
 
 frame_bottom = Frame(root)
-frame_bottom.pack(expand=False, anchor=S, fill=BOTH, padx=3, pady=3)
+frame_bottom.pack(expand=False, anchor=N, fill=BOTH, padx=3, pady=3)
 
+# file selector
 file_label = Label(frame_top, text='Target file:')
 file_label.pack(side=LEFT)
 
-file_name = Entry(frame_top)
-file_name.insert(0, 'Select a file...')
-file_name.configure(state=DISABLED)
-file_name.pack(expand=True, side=LEFT, fill=X)
+entry_filename = Entry(frame_top)
+entry_filename.insert(0, 'Select a file...')
+entry_filename.configure(state=DISABLED)
+entry_filename.pack(expand=True, side=LEFT, fill=X)
 
 btn_choose = Button(frame_top, text='Choose', command=choose_file)
 btn_choose.pack(side=RIGHT)
 
-txt_output = ConsoleText(frame_mid, font="Courier 14")
+# console output
+txt_output = ConsoleText(frame_log, font="Courier 14", height=12)
 txt_output.pack(expand=True, fill=BOTH)
 
+# Tracks treeview
+treeview_tracks = Treeview(frame_tracks)
+treeview_tracks.pack(expand=True, fill=BOTH)
+
+# Process button
 btn_process = Button(frame_bottom, text='Process', command=test)
 btn_process.pack(side=RIGHT)
 
